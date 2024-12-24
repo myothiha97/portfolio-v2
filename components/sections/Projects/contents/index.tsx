ea5365e507d5916dba3ad9f3cc444c5e376ee9c8
Myo Thiha Kyaw <mthk97.dev@gmail.com>
2024-12-24 15:04:51 +0700

    feat(Projects): convert image format to webp

--- components/sections/Projects/contents/index.tsx
+++ components/sections/Projects/contents/index.tsx
@@ -6,7 +6,7 @@ export const SevenElevenContents = () => {
       link="https://www.7-eleven.com.sg"
       title="7-Eleven Singapore"
       duration="Jan 2023 - Nov 2023"
-      imgSrc="/projects/7-eleven.jpg"
+      imgSrc="/projects/7-eleven.webp"
       description="7-Eleven Singapore is a website built for 7-eleven which was founded in 1927, is the world's largest convenience store chain. The name “7-Eleven,” introduced in 1946, reflected its extended hours (7:00 am to 11:00 pm). By the 1960s, it pioneered 24-hour operations. Today, it leads global convenience retail with over 67,000 stores in 21 countries.
 Built with modern tools like Next.js, React, TypeScript, Tailwind CSS, React Query, and Recoil."
     />
@@ -21,7 +21,7 @@ export const YogaMovementContents = () => {
       duration="May 2023 - Oct 2023"
       description="Yoga Movement is a web application which provide online booking service for various yoga programs.
 The application is built using modern technologies, including React, TypeScript, Sass, and Redux."
-      imgSrc="/projects/yoga-movement-min.png"
+      imgSrc="/projects/yoga-movement-min.webp"
     />
   );
 };
@@ -35,7 +35,7 @@ export const TrifectaContents = () => {
       description="Trifecta Singapore is a dynamic website developed for Trifecta, offering services such as skating, surfing, and more in Singapore.
 The website features online booking capabilities and provides comprehensive information about the business.
 It is built with cutting-edge technologies, including Next.js, React.js, TypeScript, Tailwind CSS, Recoil, and React Query."
-      imgSrc="/projects/trifecta-min.png"
+      imgSrc="/projects/trifecta-min.webp"
     />
   );
 };
@@ -47,7 +47,7 @@ export const EsExpressContents = () => {
       title="Express Extraspace Asia"
       duration="Mar 2024 - May 2024"
       description="Extra Space Express is a fully automated, self-service storage solution in Singapore, offering 24/7 access. Through the app, users can browse, register, and access storage instantly. Options include general and air-conditioned wine storage, etc."
-      imgSrc="/projects/es-express-1-min.png"
+      imgSrc="/projects/es-express-1-min.webp"
     />
   );
 };
@@ -60,7 +60,7 @@ export const RezervContents = () => {
       duration="Feb 2022 - Jun 2022"
       description="Rezerv is a comprehensive management software for service-based businesses in industries like fitness, wellness, education, arts, healthcare, hospitality, and maintenance.
 The platform is built using modern technologies, including Next.js, React.js, TypeScript, Tailwind CSS, Material-UI, Styled-Components, React Query, Redux and Recoil"
-      imgSrc="/projects/rezerv-min.png"
+      imgSrc="/projects/rezerv-min.webp"
     />
   );
 };
@@ -74,7 +74,7 @@ export const NeurogleeContents = () => {
       description="Neuroglee Health is an organization specializing in professional cognitive healthcare services.
 The website features a user-friendly interface, offering comprehensive information about cognitive health and related services.
 It is developed using modern technologies, including Next.js, React.js, TypeScript, Tailwind CSS, and Recoil."
-      imgSrc="/projects/neuroglee-min.png"
+      imgSrc="/projects/neuroglee-min.webp"
     />
   );
 };
@@ -86,7 +86,7 @@ export const AIAMyanmarContents = () => {
       title="AIA Myanmar CMS"
       duration="Jan 2024 - May 2024"
       description="IA Myanmar is a Myanmar-based insurance company offering a range of services, including life, education, and health insurance.The AIA Myanmar CMS is a content management portal designed to streamline content handling. It is built using modern technologies such as Next.js, React.js, TypeScript, React Query, and Recoil."
-      imgSrc="/projects/aia-min.png"
+      imgSrc="/projects/aia-min.webp"
     />
   );
 };
@@ -99,7 +99,7 @@ export const RWSContents = () => {
       duration="Feb 2024 - May 2024"
       description="The RWS Singapore website was developed for one of Singapore's top destinations on Sentosa, showcasing attractions like Universal Studios Singapore, S.E.A. Aquarium, Adventure Cove Waterpark, six unique hotels, and exceptional dining experiences.
 The project encompassed the website, CMS, and mobile app, leveraging modern technologies such as Next.js, React.js, AngularJS, React Query, Recoil, TypeScript, Node.js, and Sass."
-      imgSrc="/projects/rws-min.png"
+      imgSrc="/projects/rws-min.webp"
     />
   );
 };
@@ -113,7 +113,7 @@ export const KcutsContents = () => {
       description="Kcuts is a korean hair-salon based in Singapore with many outlets.
 Kcuts CMS is a featureful portal to manage contents of different outlets of Kcuts.
 The portal was built by Typescript, Sass, React, React Query and Redux."
-      imgSrc="/projects/kcuts-1-min.png"
+      imgSrc="/projects/kcuts-1-min.webp"
     />
   );
 };
@@ -124,7 +124,7 @@ export const SPOTVContents = () => {
       title={"SPOTV-NOW"}
       link="https://web-sea-stg.spotvnow.com"
       duration="Apr 2024 - Sep 2024"
-      imgSrc="/projects/spotv-min.png"
+      imgSrc="/projects/spotv-min.webp"
       description="SPOTV-NOW is a web app delivering South Korea's top sports streaming service to Southeast Asia, offering exclusive content live and on replay.
 It features live streams and playback of various sports channels, multiple subscription options (e.g., Seasonal Pass, Premium, Base), and an integrated online payment system for direct subscriptions. Authentication and authorization (login/logout) are also supported.
 The platform supports five languages: English, Indonesian, Malaysian, Chinese, and Thai. 
