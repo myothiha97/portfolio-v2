8b252191f3c23187ba46eb52b5d8c2c1d8d9ab50
Myo Thiha Kyaw <mthk97.dev@gmail.com>
2024-12-25 10:47:27 +0700

    feat: change site title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Myo Thiha Kyaw - Senior Web developer</title>
+      </Head>
+
       <body className="antialiased">
         <Main />
         <NextScript />
