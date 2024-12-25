4e175f7a0f73604e4089c090e5fc761acc91f6e2
Myo Thiha Kyaw <mthk97.dev@gmail.com>
2024-12-25 14:45:01 +0700

    fix: missing className props in slide in motion component

--- components/Animations/motions/SlideIn.tsx
+++ components/Animations/motions/SlideIn.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { motion, HTMLMotionProps } from "framer-motion";
 import { slideInMotionVariants } from "@/libs/utils/ui";
+import { cn } from "@/libs/utils/styles";
 
 type Props = HTMLMotionProps<"div">;
 
-const SlideIn = ({ children, ...res }: Props) => {
+const SlideIn = ({ children, className, ...res }: Props) => {
   return (
     <motion.div
-      className="sm:hidden"
+      className={cn(className)}
       variants={slideInMotionVariants}
       initial="slideOut"
       whileInView={"slideIn"}
