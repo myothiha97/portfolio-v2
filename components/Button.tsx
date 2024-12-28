78d9704bd7d867c0c1e58d9211705ed0b2771af3
Myo Thiha Kyaw <mthk97.dev@gmail.com>
2024-12-28 10:41:13 +0700

    feat: update meta information

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,34 +1,30 @@
-import {cn} from "@/libs/utils/styles";
-import React, {ComponentProps, useState} from "react";
-import {BorderBeam} from "./ui/border-beam";
+import { cn } from "@/libs/utils/styles";
+import React, { ComponentProps, useState } from "react";
+import { BorderBeam } from "./ui/border-beam";
 
-interface ButtonProps extends ComponentProps<"button"> {
-}
+interface ButtonProps extends ComponentProps<"button"> {}
 
-const Button = ({children, ...res}: ButtonProps) => {
-    const [hover, setHover] = useState(false);
-    return (
-
-        <button
-            {...res}
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}
-            className={cn("relative", res.className)}
-            {...res}
-        >
-
-            <BorderBeam
-                className="w-14 h-14 border-beam rounded-full absolute inset-0 z-[1]"
-                borderWidth={2}
-                colorFrom="#f5f1ce"
-                colorTo="#a6a386"
-                duration={2}
-                anchor={40}
-            />
-            <span className="relative z-[2] flex justify-center">{children}</span>
-        </button>
-
-    );
+const Button = ({ children, ...res }: ButtonProps) => {
+  const [hover, setHover] = useState(false);
+  return (
+    <button
+      {...res}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className={cn("relative", res.className)}
+      {...res}
+    >
+      <BorderBeam
+        className="w-14 h-14 border-beam rounded-full absolute inset-0 z-[1]"
+        borderWidth={2}
+        colorFrom="#f5f1ce"
+        colorTo="#a6a386"
+        duration={2}
+        anchor={40}
+      />
+      <span className="relative z-[2] flex justify-center">{children}</span>
+    </button>
+  );
 };
 
 export default Button;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,14 @@ export default function Document() {
       <Head>
         <title>Myo Thiha Kyaw - Senior Web developer</title>
         <link rel="icon" href="/android-chrome-192x192.png" />
+        <meta
+          name="description"
+          content="Myo Thiha Kyaw, Senior web developer with 5 years of experience in web development.Proficient in React, Next.js, Node.js, Express.js, MongoDB, and other web technologies."
+        />
+        <meta
+          name="keywords"
+          content="myo thiha kyaw,myo thiha,senior web developer,web developer,software engineer,react,nextjs,nodejs,python,typescript,nestjs"
+        />
       </Head>
 
       <body className="antialiased">
